c9f977777b781cf7c4327a805b2c5e638440a03d
MSkwarWSD <MSkwar@wsdvt.org>
2024-03-26 13:36:22 -0400

    a good shoddy working state

--- popup.js
+++ popup.js
@@ -1,23 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     var playBtn = document.getElementById('playStopBtn');
+    var masterVolumeSlider = document.getElementById('masterNoiseVolume');
     var noiseButtons = document.querySelectorAll('[id$="NoiseButton"]');
     var volumeSliders = document.querySelectorAll('[id$="NoiseVolume"]');
 
     var noises = [
-        { buttonId: 'whiteNoiseButton', audio: new Audio('noise/03-White-Noise-60min.mp3') },
-        { buttonId: 'dryerNoiseButton', audio: new Audio('noise/04-Dryer-60min.mp3') },
-        { buttonId: 'fan1NoiseButton', audio: new Audio('noise/05-Fan-60min.mp3') },
-        { buttonId: 'fan2NoiseButton', audio: new Audio('noise/33-Fan-60min.mp3') },
-        { buttonId: 'pinkNoiseButton', audio: new Audio('noise/07-PinkNoise-60min.mp3') },
-        { buttonId: 'rain1NoiseButton', audio: new Audio('noise/08-Rain-60min.mp3') },
-        { buttonId: 'rain2NoiseButton', audio: new Audio('noise/42-Rain-60min.mp3') },
-        { buttonId: 'rain3NoiseButton', audio: new Audio('noise/44-Rain-60min.mp3') },
-        { buttonId: 'heaterNoiseButton', audio: new Audio('noise/22-Heater-60min.mp3') },
-        { buttonId: 'ocean1NoiseButton', audio: new Audio('noise/25-Ocean-60min.mp3') },
-        { buttonId: 'ocean2NoiseButton', audio: new Audio('noise/26-Ocean-60min.mp3') },
-        { buttonId: 'underwater1NoiseButton', audio: new Audio('noise/46-Underwater-60min.mp3') },
-        { buttonId: 'underwater2NoiseButton', audio: new Audio('noise/49-Underwater-60min.mp3') },
-        { buttonId: 'waterfallNoiseButton', audio: new Audio('noise/32-Waterfall-60min.mp3') }
+        { buttonId: 'whiteNoiseButton', audio: new Audio('noise/03-White-Noise-60min.mp3'), volume: 1.0 },
+        { buttonId: 'dryerNoiseButton', audio: new Audio('noise/04-Dryer-60min.mp3'), volume: 1.0 },
+        { buttonId: 'fan1NoiseButton', audio: new Audio('noise/05-Fan-60min.mp3'), volume: 1.0 },
+        { buttonId: 'fan2NoiseButton', audio: new Audio('noise/33-Fan-60min.mp3'), volume: 1.0 },
+        { buttonId: 'pinkNoiseButton', audio: new Audio('noise/07-PinkNoise-60min.mp3'), volume: 1.0 },
+        { buttonId: 'rain1NoiseButton', audio: new Audio('noise/08-Rain-60min.mp3'), volume: 1.0 },
+        { buttonId: 'rain2NoiseButton', audio: new Audio('noise/42-Rain-60min.mp3'), volume: 1.0 },
+        { buttonId: 'rain3NoiseButton', audio: new Audio('noise/44-Rain-60min.mp3'), volume: 1.0 },
+        { buttonId: 'heaterNoiseButton', audio: new Audio('noise/22-Heater-60min.mp3'), volume: 1.0 },
+        { buttonId: 'ocean1NoiseButton', audio: new Audio('noise/25-Ocean-60min.mp3'), volume: 1.0 },
+        { buttonId: 'ocean2NoiseButton', audio: new Audio('noise/26-Ocean-60min.mp3'), volume: 1.0 },
+        { buttonId: 'underwater1NoiseButton', audio: new Audio('noise/46-Underwater-60min.mp3'), volume: 1.0 },
+        { buttonId: 'underwater2NoiseButton', audio: new Audio('noise/49-Underwater-60min.mp3'), volume: 1.0 },
+        { buttonId: 'waterfallNoiseButton', audio: new Audio('noise/32-Waterfall-60min.mp3'), volume: 1.0 }
     ];
 
     // Event listener for noise buttons
@@ -28,6 +29,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 var volumeInput = document.getElementById(noise.buttonId.replace('Button', 'Volume'));
                 toggleNoise(button, noise.audio, volumeInput);
             }
+        });
+    });
+
+    // Event listener for master volume slider
+    masterVolumeSlider.addEventListener('input', function () {
+        noises.forEach(function (noise) {
+            noise.audio.volume = noise.volume * (masterVolumeSlider.value / 100);
         });
     });
 
@@ -61,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (audio.paused) {
             button.classList.remove('btn-outline-primary');
             button.classList.add('btn-primary');
-            audio.volume = volumeInput.value / 100;
+            audio.volume = volumeInput.value / 100 * (masterVolumeSlider.value / 100);
             audio.play();
         } else {
             button.classList.remove('btn-primary');
@@ -79,15 +87,19 @@ document.addEventListener('DOMContentLoaded', function () {
         playBtn.classList.remove(isPlaying ? 'btn-success' : 'btn-danger');
         playBtn.classList.add(isPlaying ? 'btn-danger' : 'btn-success');
     }
-    // Volume Slider Event Listener
+
+    // Function to update noise volume based on slider input
     function updateNoiseVolume(volumeInput) {
         var buttonId = volumeInput.id.replace('Volume', 'Button');
         var button = document.getElementById(buttonId);
         var noise = noises.find(function (n) { return n.buttonId === buttonId; });
         if (noise && button.classList.contains('btn-primary')) {
-            noise.audio.volume = volumeInput.value / 100;
+            noise.volume = volumeInput.value / 100;
+            noise.audio.volume = noise.volume * (masterVolumeSlider.value / 100);
         }
     }
+
+    // Event listener for individual noise volume sliders
     volumeSliders.forEach(function (slider) {
         slider.addEventListener('input', function () {
             updateNoiseVolume(slider);
